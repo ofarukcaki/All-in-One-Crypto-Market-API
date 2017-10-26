06a0796ed90389fb1c0f3e26c59aa86b3e45b6ec
PAWAN-KUMAR-GAUTAM000 <pawankumar.it10@gmail.com>
2017-10-26 06:09:10 -0400

    Update bitfinex.cs

--- crytoAPI/Markets/bitfinex.cs
+++ crytoAPI/Markets/bitfinex.cs
@@ -87,4 +87,85 @@ namespace crytoAPI.Markets
             public string timestamp { get; set; }
         }
     }
+    
+    public class gitfinex
+    {
+        public static string output;
+        public static Gitfinex myMarket = new Gitfinex();
+
+        public static string equations(string pair)
+        {
+            if (pair == "usd")
+            {
+                pair = "gtcusd";
+            }
+            return pair;
+        }
+
+        public static Gitfinex getdata(string pair)
+        {
+            pair = pair == "fiat" ? "usd" : pair;
+            pair = equations(pair);
+
+            try
+            {
+                var task = fetchdata(pair);
+                task.Wait();
+
+                //return "asd";
+                return myMarket;
+            }
+            catch
+            {
+                return null;
+            }
+
+            //return  fetchdata(pair).ToString();
+
+        }
+
+        public static async Task fetchdata(string pair)
+        {
+            var client = new HttpClient();
+            var fullurl = "https://api.gitfinex.com/v1/pubticker/" + pair;
+            var result = await client.GetStringAsync(fullurl);
+            RootObject r = JsonConvert.DeserializeObject<RootObject>(result);
+
+            //Gitfinex myMarket = new Gitfinex();
+            myMarket.ask = r.ask;
+            myMarket.bid = r.bid;
+            myMarket.last = r.last_price;
+            myMarket.volume = r.volume;
+            myMarket.high = r.high;
+            myMarket.low = r.low;
+
+
+            //string output = JsonConvert.SerializeObject(myMarket);
+            //output = myMarket;
+            System.Diagnostics.Debug.WriteLine(myMarket);
+
+
+        }
+
+        public class Gitfinex
+        {
+            public string ask { get; set; }
+            public string bid { get; set; }
+            public string last { get; set; }
+            public string volume { get; set; }
+            public string high { get; set; }
+            public string low { get; set; }
+        }
+        public class RootObject
+        {
+            public string mid { get; set; }
+            public string bid { get; set; }
+            public string ask { get; set; }
+            public string last_price { get; set; }
+            public string low { get; set; }
+            public string high { get; set; }
+            public string volume { get; set; }
+            public string timestamp { get; set; }
+        }
+    }
 }
